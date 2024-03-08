91241ad08c32d32b940c62c4e312937b642d0af8
humphreyokoth <humpasp@gmail.com>
2024-03-08 13:52:53 +0300

    Feate:deployment to render

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # cashfulapi
+https://cashfulapi.onrender.com
+# API documentation
+https://documenter.getpostman.com/view/14955059/2sA2xh1CTb
--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,21 @@
 
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-  user: process.env.DB_USERNAME,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432, 
-});
+// const pool = new Pool({
+//   user: process.env.DB_USERNAME,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_DATABASE,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT || 5432, 
+// });
 
 
-module.exports = pool;
+// module.exports = pool;
+
+import postgres from 'postgres'
+
+const connectionString = process.env.DATABASE_URL
+const pool = postgres(connectionString)
+
+export default pool
